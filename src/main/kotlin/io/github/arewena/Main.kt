807f27593d84622aacf046ea44d0f6af30783d21
CosyTreq <74582539+Arewena@users.noreply.github.com>
2022-08-14 20:19:18 +0900

    이슈 해결
    플레이어들끼리 보스바가 공유되는 현상 수정.

--- src/main/kotlin/io/github/arewena/Main.kt
+++ src/main/kotlin/io/github/arewena/Main.kt
@@ -17,35 +17,46 @@ import kotlin.math.roundToInt
 
 
 class Main : JavaPlugin(), Listener {
-    private val bossBar = BossBar.bossBar(Component.text("Nothing detected."), 0f, BossBar.Color.RED, BossBar.Overlay.NOTCHED_10)
+    override fun onEnable() { this.server.pluginManager.registerEvents(this, this) }
+
+    private var BossbarList = hashMapOf<Player, BossBar>()
     var task: BukkitTask? = null
-    fun initi() {
+
+    fun initi(bossBar: BossBar) {
         task = Bukkit.getScheduler().runTaskLater(this, Runnable {
-            bossBar.name(Component.text("Nothing detected."))
+            bossBar.name((Component.text("Nothing detected.")))
             bossBar.progress(0f)
         }, 100L)
     }
 
-    override fun onEnable() { this.server.pluginManager.registerEvents(this, this) }
-
     @EventHandler
     fun playerJoin(e: PlayerJoinEvent) {
-        e.player.showBossBar(bossBar)
+        val basic = BossBar.bossBar(Component.text("Nothing detected."), 0f, BossBar.Color.RED, BossBar.Overlay.NOTCHED_10)
+        BossbarList.put(e.player, basic)
+        e.player.showBossBar(basic)
     }
+
+
 
     @EventHandler
     fun hitEntity(e: EntityDamageByEntityEvent) {
         if (e.entity is LivingEntity && e.damager is Player && e.entityType != EntityType.ARMOR_STAND) {
             task?.cancel()
-            var victim = ""
-            if (e.entity is Player) { victim = (e.entity as Player).name }
-            else { victim = e.entity.name }
+
             val damage = (e.damage.toFloat() * 10.0).roundToInt() / 10f
             val victimHp = if ((e.entity as LivingEntity).health.toFloat() - damage > 0.0) { ((((((e.entity as LivingEntity).health.toFloat() - damage) * 10.0).roundToInt())) / 10.0) } else { 0.0 }
-            bossBar.name(Component.text("$victim, hp:$victimHp, dmg:$damage"))
-            if (victimHp > 0.0) { bossBar.progress(((victimHp / (e.entity as LivingEntity).maxHealth).toFloat())) }
-            else { bossBar.progress(0f) }
-            initi()
+
+            val victim =
+                if (e.entity is Player) { (e.entity as Player).name }
+                else { e.entity.name }
+
+            BossbarList[(e.damager) as Player]!!.name(Component.text("$victim, hp:$victimHp, dmg:$damage"))
+            BossbarList[(e.damager) as Player]!!.progress((victimHp / (e.entity as LivingEntity).maxHealth).toFloat())
+
+
+            if (victimHp > 0.0) {  BossbarList[(e.damager) as Player]!!.progress(((victimHp / (e.entity as LivingEntity).maxHealth).toFloat())) }
+            else {  BossbarList[(e.damager) as Player]!!.progress(0f) }
+            initi(BossbarList[(e.damager) as Player]!!)
 
         }
     }
